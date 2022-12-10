32b08393f34e40eef169f21c5995e92f92263e16
Maxun <trukhinmaksimof@mail.ru>
2022-12-11 01:23:43 +0800

    added some hover-focus styles to filter buttons

--- src/components/ui/Haeder.jsx
+++ src/components/ui/Haeder.jsx
@@ -1,13 +1,26 @@
 import "./css/header.scss";
+//import searchIcon from "../../assets/icons/search__icon.svg";
+import logo from "../../logo.svg"
 
 export default function (props) {
     return (
         <div className = "header">
             <div className = "brand">Wind BnB</div>
             <form className = "search">
-                <input className = "search-bar location" type = "text" placeholder="Search"/>
-                <input className = "search-bar guests" type = "text" placeholder="Search"/>
-                <button className = "search-btn">o</button>
+                <button className = "search-bar location" name = "search__filters">
+                    <span>Location</span>
+                    <span>Helsinki, Finland</span>
+                </button>
+                <button className = "search-bar guests" name = "search__filters">
+                    <span>Guests</span>
+                    <span>4 adults</span>
+                </button>
+                <div className = "search-btn-wrapper">
+                    <button className = "search-btn">
+
+                        <span className = "search__label">Search</span>
+                    </button>
+                </div>
                 
                 <div className = "additional-filters">
                     <div className = "filter">
