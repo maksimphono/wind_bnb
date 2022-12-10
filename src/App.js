8162b5cdb8730fd386b5e470adef1e1e7cc7b27c
Maxun <trukhinmaksimof@mail.ru>
2022-12-10 22:59:56 +0800

    created style for search fields

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './assets/global.css';
 import CardCollection from "./components/ui/CardCollection.jsx";
+import Header from "./components/ui/Haeder.jsx";
 
 function App() {
   return (
-    <CardCollection />
+    <>
+      <Header />
+      <CardCollection />
+    </>
   );
 }
 
---
+++ src/components/ui/Haeder.jsx
@@ -0,0 +1,24 @@
+import "./css/header.scss";
+
+export default function (props) {
+    return (
+        <div className = "header">
+            <div className = "brand"></div>
+            <form className = "search">
+                <input className = "search-bar location" type = "text" placeholder="Search"/>
+                <input className = "search-bar guests" type = "text" placeholder="Search"/>
+                <button className = "search-btn">o</button>
+                
+                <div className = "additional-filters">
+                    <div className = "filter">
+                        <input checked type = "radio" name = "radio1"/>  
+                    </div>
+                    <div className = "filter">
+                        <input type = "radio" name = "radio1"/>
+                    </div>
+                </div>
+                
+            </form>
+        </div>
+    )
+}
