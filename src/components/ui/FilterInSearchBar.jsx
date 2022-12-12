1e41a4098706aec6dc698e28971c701ca9b3bd57
Maxun <trukhinmaksimof@mail.ru>
2022-12-13 00:24:13 +0800

    made some togglers with jquery and react events

--- src/components/ui/FilterInSearchBar.jsx
+++ src/components/ui/FilterInSearchBar.jsx
@@ -1,16 +1,49 @@
+import {useRef, useMemo, useEffect} from "react";
+import $ from "jquery";
+
 export default function(props) {
+    const selectedItemRef = useRef();
+    const selectionDivRef = useRef();
+    let $selectedItemSpan;
+    let $selectionDiv;
+
+    useEffect(() => {
+        $selectedItemSpan = $(selectedItemRef.current);
+        $selectionDiv = $(selectionDivRef.current);
+    }, []);
+
+    const onSelectItem = ({target}) => {
+        const $target = $(target);
+        
+        $selectedItemSpan.text($target.text());
+    }
+
+    const onToggleSelection = (event) => {
+        const cssOpacity = Number($selectionDiv.css("opacity"));
+
+        //console.log(cssOpacity);
+        $selectionDiv.css({
+                "opacity": "" + Number(!cssOpacity),
+            }
+        );
+        $selectionDiv.find("ul.selection").css("height", cssOpacity? "0px": "max-content");
+    }
+    
     return (
-        <div className = "search-bar location" name = "search__filters">
-            <div className = "summary">
+        <div className = "search-bar" name = "search__filters">
+            <div className = "summary" onClick = {onToggleSelection}>
                 <span>{props.title}</span>
-                <span>Subtitle</span>
+                <span ref = {selectedItemRef}>{props.list[0] || "Subtitle"}</span>
             </div>
-            <div className = "selection">
+            <div 
+                ref = {selectionDivRef} 
+                className = "selection"
+            >
                 <ul className = "selection">
                     {props.list && props.list.map(
-                        (elem) => (
-                            <li>
-                                <label>{elem.toString()}</label>
+                        (item, i) => (
+                            <li key = {i} onClick = {onSelectItem}>
+                                {item.toString()}
                             </li>
                         ))
                     }
