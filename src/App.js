df3e19eae61637468db9f4f12f71205dd38186aa
Maxun <trukhinmaksimof@mail.ru>
2022-12-15 03:04:26 +0800

    started to create simple style for CardFullView

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './assets/global.css';
 import CardCollection from "./pages/CardsCollection/CardCollection.jsx";
 import Header from "./components/ui/Haeder.jsx";
+import CardFullView from "./pages/CardFullView/index.jsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <CardCollection />
+      <CardFullView />
     </>
   );
 }
 
 export default App;
+
+/*
+<>
+      <Header />
+      <CardCollection />
+    </>
+*/
--- src/pages/CardFullView/index.jsx
+++ src/pages/CardFullView/index.jsx
@@ -1,9 +1,12 @@
+import "./css/cardFullView.scss";
+
 export default function() {
     return (
         <div className = "card__full__view">
             <div className = "card__title">
                 <h2>Seaview and Thai temple view pool villa</h2>
                 <span className = "star__number">4.4 stars</span>
+                <span className = "superhost">Superhost</span>
                 <span className = "adress">Ko Samui, Chang Wat Surat Thani, Thailand</span>
             </div>
             <div className = "images">
@@ -14,19 +17,19 @@ export default function() {
                 <img src="" alt="" />
             </div>
             <div className = "owner__info">
-                <p className = "info"></p>
+                <p className = "info">Entire house is owed by Lisa</p>
                 <img src="" alt="" className = "avatar" />
             </div>
             <div className = "reserve__card">
                 <div className = "reserve__card__title">
-                    <span>$4,998/night</span>
+                    <span>$4,998 <span>/ night</span></span>
                     <span>4.4 stars</span>
                 </div>
                 <form className = "set__in-out__date">
                     <input type="date" className = "checkin__date" />
                     <input type="date" className = "checkout__date" />
-                    <div style = {{background: "red"}}></div>
-                    <div style = {{background: "#ada"}}></div>
+                    <div className = "adults__number__setting" style = {{background: "red"}}></div>
+                    <div className = "children__number__setting" style = {{background: "#ada"}}></div>
                 </form>
             </div>
         </div>
