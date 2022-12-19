262909ac5b5dd99b3d87caf6e642ec430633b7b3
Maxun <trukhinmaksimof@mail.ru>
2022-12-20 02:09:03 +0800

    created serializers, urls and views. Prepare to add Cross-origin resource sharing(CORS), (probably will switch to a new branch)

--- backend/windbnb/api/admin.py
+++ backend/windbnb/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import WindBnBCard
+from .models import WindBnBCardModel
 
-admin.site.register(WindBnBCard)
+admin.site.register(WindBnBCardModel)
--- backend/windbnb/api/views.py
+++ backend/windbnb/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import WindBnBCardModel
+from .serializers import WindBnBCardSerializer
 
 # Create your views here.
+
+class CardsCollection(generics.ListCreateAPIView):
+    queryset = WindBnBCardModel.objects.all()
+    serializer_class = WindBnBCardSerializer
+
+class WindBnBCard(generics.RetrieveUpdateDestroyAPIView):
+    queryset = WindBnBCardModel.objects.all()
+    serializer_class = WindBnBCardSerializer
