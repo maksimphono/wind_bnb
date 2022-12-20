bf2d80a0bbbc9e6325dd52c05697cfeb66530e6f
Maxun <trukhinmaksimof@mail.ru>
2022-12-20 21:56:47 +0800

    resolved problem with missing netloc, allowed all hosts

--- backend/windbnb/windbnb/settings.py
+++ backend/windbnb/windbnb/settings.py
@@ -58,9 +58,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    "localhost:3000/"
-)
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'windbnb.urls'
 
