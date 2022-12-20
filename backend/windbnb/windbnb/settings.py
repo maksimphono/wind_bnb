27c6bd89aa4a9c534be744676bd8a72672a2b5f2
Maxun <trukhinmaksimof@mail.ru>
2022-12-20 20:12:51 +0800

    added some simple CORS

--- backend/windbnb/windbnb/settings.py
+++ backend/windbnb/windbnb/settings.py
@@ -38,10 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "corsheaders",
     "api",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES" : ['rest_framework.permissions.AllowAny']
+}
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "localhost:3000/"
+)
 
 ROOT_URLCONF = 'windbnb.urls'
 
