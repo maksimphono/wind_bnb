fe2b1dc357b32164c2ba4dad818e4b49a1403dc5
Maxun <trukhinmaksimof@mail.ru>
2022-12-22 00:20:07 +0800

    added simple api data maintaining

--- frontend/src/pages/CardsCollection/CardCollection.jsx
+++ frontend/src/pages/CardsCollection/CardCollection.jsx
@@ -21,18 +21,18 @@ export default function(props) {
         <>
             <h1 className = "stay-location">Stay in Finland</h1>
             <div className = "card-collection">
-                <Link to = "/full/1">
-                    <Card name = "Hotel 1" description = "Desc 1" isSuperHost starsRate = {5.6} />
-                </Link>
-                <Link to = "/full/2">
-                    <Card name = "Hotel 2" description = "Desc 2" isSuperHost starsRate = {5.6} />
-                </Link>
-                <Card name = "Hotel 2" description = "" isSuperHost starsRate = {3.6} />
-                <Card name = "Hotel 3" description = "Desc 2" starsRate = {2.6} />
-                <Card name = "Hotel 4" description = "" starsRate = {4.6} />
-                <Card name = "Hotel 5." description = "" isSuperHost starsRate = {5.6} />
-                <Card name = "Hotel 6" description = "" starsRate = {5.6} />
+                {data.map(item => 
+                    <Link key = {item.id} to = {"/full/" + item.id}>
+                        <Card name = {item.title} description = {item.description} isSuperHost starsRate = {5.0} />
+                    </Link>
+                )}
             </div>
         </>       
     )
 }
+
+/*
+<Link to = "/full/1">
+                    <Card name =  description = "Desc 1" isSuperHost starsRate = {5.6} />
+                </Link>
+*/
