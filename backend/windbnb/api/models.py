9aafd80b7654049cf1ed04abea30eff4b35ae4be
Maxun <trukhinmaksimof@mail.ru>
2022-12-28 01:44:00 +0800

    prepare to add filters by location

--- backend/windbnb/api/models.py
+++ backend/windbnb/api/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-<<<<<<< HEAD
-class Location(models.Model):
-    country = models.CharField(max_length = 255)
-    city = models.CharField(max_length = 255)
-
-    def __str__(self):
-        return self.country + ", " + self.city
-
-=======
->>>>>>> 903435210e5c955cae685c8cf2682bde41ee474e
 class WindBnBCardModel(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
@@ -24,11 +14,8 @@ class WindBnBCardModel(models.Model):
     nocancelationfee = models.BooleanField(default = False)
     securitysystems = models.BooleanField(default = False)
 
-<<<<<<< HEAD
-    location = models.ForeignKey(Location, on_delete = models.CASCADE, default = 0)
+    location = models.CharField(max_length=255, default = "")
 
-=======
->>>>>>> 903435210e5c955cae685c8cf2682bde41ee474e
     preview_image = models.ImageField(upload_to="pictures", default = "media/pictures/default_house.png")
 
     # will design models relationship later
